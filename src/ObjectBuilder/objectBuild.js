0e8262399bc1c112bb09d0f2bda437e2bf3a938a
EthanPKlein <eklein@icct.com>
2016-03-27 22:55:35 -0400

    Adding object literal example

--- src/ObjectBuilder/objectBuild.js
+++ src/ObjectBuilder/objectBuild.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+// object literal
+var car = {
+	wheelCount: 4,
+	seats: {
+		frontLeft: "empty",
+		frontRight: "empty",
+		backLeft: "empty",
+		backRight: "empty"
+	},
+	passeners: [
+		"Bob",
+		"Anne"
+	]
+};
+
+console.log(car);
